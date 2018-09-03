6b519eead1b4c796c187d91f36bcc82f8a400a99
agnipanda <pandaagni@outlook.com>
2018-09-04 00:41:50 +0530

    repositioned logo

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
+router.get('/' ,function(req,res){
+  res.redirect('home');
+})
 router.get('/chat' ,function(req,res){
   res.render('chatbox');
 })
 router.get('/home',function(req,res){
   res.render('index');
 });
+router.post('/home',function(req,res){
+  res.redirect('chat');
+})
 module.exports = router;
