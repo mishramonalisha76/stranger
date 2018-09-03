c3ce539438a0e2576d063a916ef7915e736f2ce9
mishramonalisha76 <mishramonalisha76@gmail.com>
2018-09-03 18:10:56 +0530

    home page added

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+
 router.get('/chat' ,function(req,res){
   res.render('chatbox');
 })
+router.get('/home',function(req,res){
+  res.render('index');
+});
 module.exports = router;
