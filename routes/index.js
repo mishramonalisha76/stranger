3d5ed1de647b9f73a0763cf6809e501fa2efaaee
mishramonalisha76 <mishramonalisha76@gmail.com>
2018-09-24 21:43:10 +0530

    added login file

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,21 @@ var router = express.Router();
 /* GET home page. */
 router.get('/' ,function(req,res){
   res.redirect('home');
-})
+});
+router.get('/login' ,function(req,res){
+  res.render('login');
+});
 router.get('/chat' ,function(req,res){
   res.render('chatbox');
-})
+});
 router.get('/home',function(req,res){
   res.render('index');
 });
 router.post('/home',function(req,res){
   res.redirect('chat');
 
-})
+});
 router.get('/homepage' ,function(req,res){
   res.render('page');
-})
+});
 module.exports = router;
