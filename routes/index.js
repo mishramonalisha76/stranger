e4b5308c940fb26157c6b9f02fd229ca9a16f4b4
Rajashree Parhi <rajashreeparhi23@gmail.com>
2018-09-24 21:52:40 +0530

    login

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,18 @@ router.get('/' ,function(req,res){
 });
 router.get('/login' ,function(req,res){
   res.render('login');
-});
+})
 router.get('/chat' ,function(req,res){
   res.render('chatbox');
-});
+})
 router.get('/home',function(req,res){
   res.render('index');
-});
+})
 router.post('/home',function(req,res){
   res.redirect('chat');
 
 });
 router.get('/homepage' ,function(req,res){
   res.render('page');
-});
+})
 module.exports = router;
