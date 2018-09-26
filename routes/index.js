b70704f892a51b60dbcd2d8fa629f3036e51474c
mishramonalisha76 <mishramonalisha76@gmail.com>
2018-09-26 16:59:26 +0530

    integrated socket

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var c=0;
 var io = require('socket.io').listen(3001, {log: false});
 /* GET home page. */
 router.get('/' ,function(req,res){
@@ -13,10 +13,7 @@ router.get('/chat' ,function(req,res){
   res.render('chatbox');
 })
 
-router.post('/login',function(req,res){
-  res.redirect('/chat');
 
-});
 router.post('/homepage',function(req,res){
   res.redirect('/login');
 
@@ -24,15 +21,49 @@ router.post('/homepage',function(req,res){
 router.get('/homepage' ,function(req,res){
   res.render('page');
 })
+router.post('/login',function(req,res){
+  
+  
+  if(c%2==0)
+  {
+  var user1=req.body.username;
+  res.redirect('/chat');
+  console.log(user1);
+  console.log("test");
+  
+  var msg;
+  io.on('connection', function(socket) {
+    console.log('A user connected');
+     
+    socket.on('msg', function(msg) {
+      //Send message to everyone
+      var data=user1+':'+msg;
+       
+      io.sockets.emit('newmsg', data);
+   })
+  });
+  c++;
+    }
+  else
+  {
+  var user2=req.body.username;
+  res.redirect('/chat');
+  console.log(user2);
+console.log("test");
 
-
+var msg;
 io.on('connection', function(socket) {
   console.log('A user connected');
    
-  socket.on('msg', function(data) {
+  socket.on('msg', function(msg) {
     //Send message to everyone
+    var data=user2+':'+msg;
+     
     io.sockets.emit('newmsg', data);
  })
+});
+c++;
+  }
 
 });
 module.exports = router;
